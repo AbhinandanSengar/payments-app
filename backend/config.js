2e0b59f84e39dc09a45b5c2ae865b2660c4dd8ed
AbhinandanSengar <abhinandansengar@gmail.com>
2025-06-08 17:36:47 +0530

    Added init files

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "abhinandan@01";
+
+module.exports = { JWT_SECRET };
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,23 @@
+require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
+const mainRouter = require("./routes/index");
+const { default: mongoose } = require("mongoose");
 
+const app = express();
+const PORT = process.env.PORT;
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", mainRouter);
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+        console.log(`Server is listening on PORT ${PORT}`)
+    });
+}
+
+main();
